7c456f291e299bb6d3a8d279a4064c23b7eea468
✡  Brown <dbbrwnn@amazon.com>
2019-08-08 16:47:19 +0200

    Continued work on Django command

--- pyclops/commands/django.py
+++ pyclops/commands/django.py
@@ -1,6 +1,11 @@
-import sys, subprocess, click, boto3
+import os
+import sys
+import subprocess
+import click
+import boto3
 
 from pyclops.lib.io import process
+from pyclops.lib.docker import docker
 
 
 DEFAULT_DJANGO_TEMPLATE = "https://github.com/Stavatech/Django-Template.git"
@@ -17,22 +22,29 @@ def django():
 @click.option('--domain', prompt='Web domain of service', help='The domain that the service will be located at')
 @click.option('--template', default=DEFAULT_DJANGO_TEMPLATE, help='The template git repository')
 @click.option('--provider', default=DEFAULT_CLOUD_PROVIDER, type=click.Choice(['aws', 'azure']), help='The cloud provider to deploy the app to')
-@click.argument('working-directory', type=click.Path())
-def create_stack(name, domain):
+@click.argument('working-dir', type=click.Path())
+def create_stack(name, domain, template, provider, working_dir):
     """ Command that sets up a Django stack in the cloud """
     # Steps:
     # 1) clone git repo
-    out, err, returncode = process.run("git clone %s" % DEFAULT_DJANGO_TEMPLATE)
+    os.makedirs(working_dir)
+
+    out, err, returncode = process.run("git clone %s %s" % (DEFAULT_DJANGO_TEMPLATE, working_dir))
     if returncode != 0:
         print (err)
         sys.exit(returncode)
 
-    # 2) create Docker image repo
+    # 2) replace cfn placeholders
 
-    # 3) build and publish docker image to image repo
+    # 3) push to new repo
 
-    
-    # 4) replace cfn placeholders
+    # 3) create Docker image repo
+    provider_lib = __import__('pyclops.lib.aws', fromlist='')
+    repo = provider_lib.docker.create_repo(name)
+
+    # 4) build and publish docker image to image repo
+    process.run('STAGE=production %s/docker/build.sh' % working_dir)
+
     # 5) create cfn service stack
     # 6) create cfn pipeline stack
 
