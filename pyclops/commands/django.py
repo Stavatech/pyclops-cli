aef9cb21227fb27b43c0bfc6e08710e690525490
✡  Brown <dbbrwnn@amazon.com>
2019-08-08 16:43:39 +0200

    Started implementing Django command

--- pyclops/commands/django.py
+++ pyclops/commands/django.py
@@ -1,20 +1,37 @@
-import click
+import sys, subprocess, click, boto3
+
+from pyclops.lib.io import process
+
+
+DEFAULT_DJANGO_TEMPLATE = "https://github.com/Stavatech/Django-Template.git"
+DEFAULT_CLOUD_PROVIDER = "aws"
 
 
 @click.group()
 def django():
-    """Set up a Python Django application in the cloud"""
+    """ Set up a Python Django application in the cloud """
 
 
 @click.command()
 @click.option('--name', prompt='Stack name', help='The name that will be given to the stack')
-@click.option('--domain', prompt='Web domain of service', help='The dmain that the service will be located at')
+@click.option('--domain', prompt='Web domain of service', help='The domain that the service will be located at')
+@click.option('--template', default=DEFAULT_DJANGO_TEMPLATE, help='The template git repository')
+@click.option('--provider', default=DEFAULT_CLOUD_PROVIDER, type=click.Choice(['aws', 'azure']), help='The cloud provider to deploy the app to')
+@click.argument('working-directory', type=click.Path())
 def create_stack(name, domain):
-    """Simple program that greets NAME for a total of COUNT times."""
+    """ Command that sets up a Django stack in the cloud """
     # Steps:
     # 1) clone git repo
-    # 2) create ECR repo
-    # 3) publish docker image to ECR
+    out, err, returncode = process.run("git clone %s" % DEFAULT_DJANGO_TEMPLATE)
+    if returncode != 0:
+        print (err)
+        sys.exit(returncode)
+
+    # 2) create Docker image repo
+
+    # 3) build and publish docker image to image repo
+
+    
     # 4) replace cfn placeholders
     # 5) create cfn service stack
     # 6) create cfn pipeline stack
