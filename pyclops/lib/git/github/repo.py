5028ccb6a168b02c3061d9881989296b35c27be6
✡  Brown <dbbrwnn@amazon.com>
2019-08-30 17:41:17 +0200

    Started generate-project command

--- pyclops/lib/git/github/repo.py
+++ pyclops/lib/git/github/repo.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from pyclops.lib.git.github.template import Template
+from pyclops.lib.git.template import Template
 from pyclops.lib.git.github.provider import GithubProvider
 from pyclops.lib.git.repo import Repository
 
@@ -8,7 +8,7 @@ from pyclops.lib.git.repo import Repository
 def create_repo(git_provider:GithubProvider, owner:str, name:str, is_org:bool=False, optional_params:dict={}, template:Template=None) -> Repository:
     if template is not None:
         body = {"name": name, "owner": owner, **optional_params}  
-        relative_url = "repos/%s/%s/generate" % (template.template_owner, template.template_repo)
+        relative_url = "repos/%s/%s/generate" % (template.template_owner, template.template_name)
     else:
         body = {"name": name, **optional_params}        
         if is_org:
@@ -17,12 +17,17 @@ def create_repo(git_provider:GithubProvider, owner:str, name:str, is_org:bool=Fa
             relative_url = "%s/repos" % owner
     
     headers = {
-        "Authorization": "token %s" % git_provider.credentials
+        "Authorization": "token %s" % git_provider.credentials,
+        "Accept": "application/vnd.github.baptiste-preview+json"
     }
 
     url = os.path.join(git_provider.api_endpoint, relative_url)
     
     response = requests.post(url, json=body, headers=headers)
-    repo = response.json()
-    
-    return Repository(owner=repo['owner']['login'], name=repo['name'], git_provider=git_provider, location=repo['html_url'])
+    if response.status_code == 200:
+        repo = response.json()
+        return Repository(owner=repo['owner']['login'], name=repo['name'], git_provider=git_provider, location=repo['html_url'])
+    else:
+        error = response.json()
+        error_arr = [error['message']] + error['errors']
+        raise RuntimeError("\n- ".join(error_arr))    
--- pyclops/lib/git/github/template.py
+++
@@ -1,4 +0,0 @@
-class Template(object):
-    def __init__(self, template_owner, template_repo):
-        self.template_owner = template_owner
-        self.template_repo = template_repo
