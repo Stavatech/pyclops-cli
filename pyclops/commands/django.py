007174bbe3f82d52d7569cf5d1257a5765143476
davidbrownza <davidbrownza@outlook.com>
2020-02-02 14:03:52 +0200

    Refactor - moved CFN commands under AWS and added common generate_project function

--- pyclops/commands/django.py
+++ pyclops/commands/django.py
@@ -1,11 +1,9 @@
-import os
 import sys
 import click
 
-from pyclops.lib.jinja import jinja
 from pyclops.lib.git.github.provider import GithubProvider
 from pyclops.lib.git.repo import Repository
-from pyclops.lib.io.build import clean
+from pyclops.lib.projects.project import generate_from_template
 
 
 DEFAULT_DJANGO_TEMPLATE = Repository(
@@ -32,32 +30,12 @@ def django():
 @click.argument('working-dir', type=click.Path())
 def generate_project(project_name:str, git_owner:str, is_org:bool, branch:str, template:Repository, working_dir:str):
     """ Generates a new project from a Django template """   
-    # Steps:    
-    # 1) copy template repo
-    template_dir = os.path.join(working_dir, "template")
-    template.clone(local_dir=template_dir)
-
-    new_repo = template.copy_to_new(owner=git_owner, new_repo_name=project_name, is_org=is_org)
-
-    clean(path=template_dir)
-
-    # 2) clone new repo
-    new_repo.clone(local_dir=working_dir)
-
-    # 3) run jinja preprocessor to set project_name
     params = {
         'project_name': project_name,
         'git_owner': git_owner,
         'git_branch': branch
     }
-    jinja.process_dir(path=working_dir, params=params, in_place=True, suffix=".pyclops", remove_suffix=True)
-
-    # 3) push the processed repo to remote
-    new_repo.add(path=".")
-    new_repo.commit(commit_message="Generated project according to parameters")
-    new_repo.push()
-
-    print("\nGenerated repository: %s" % new_repo.html_url)
+    generate_from_template(project_name, git_owner, is_org, branch, template, params, working_dir)
 
 
 django.add_command(generate_project)
